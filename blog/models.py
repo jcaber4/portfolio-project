6bd41d71ec75b675751960c50582c19a84b8941a
John Abercrombie <jabercrombie4th@gmail.com>
2020-04-16 19:57:29 -0700

    Added Bootstrap and Jobs to homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=40)
+    date = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    body = models.CharField(max_length=300)
