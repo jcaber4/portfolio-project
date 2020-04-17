17ac51f2fab5f1dd1d9ef53e68e7b3c709a61ade
John Abercrombie <jabercrombie4th@gmail.com>
2020-04-17 02:26:42 -0700

    local settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'postdbpass',
+        'PASSWORD': 'Arsenal97',
         'HOST': 'localhost',
         'PORT': '5432',
     }
